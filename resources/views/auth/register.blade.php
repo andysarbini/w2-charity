9c6764ca94ffc5ad7480369d99d64b19a10b76df
andysarbini <kangandy09@gmail.com>
2023-08-08 10:55:07 +0700

    21. Update Halaman Registrasi
    https://www.youtube.com/watch?v=uPPAWSoqgHs&list=PLaN75JfoGz0Ovq-T54ala9QCKBhtlNtYk&index=21

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,94 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.guest')
 
-        <x-jet-validation-errors class="mb-4" />
+@section('title', 'Register')
+    
+    @section('content')
+        <div class="container-fluid">
+            <div class="row">
+                <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+                    
+                    <div class="col-md-8 col-lg-6">
+                        <div class="login d-flex align-items-center py-5">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-9 col-lg-8 mx-auto">
+                                        <a href="{{ url('/') }}">
+                                            <img src="{{ asset('/img/logo.png') }}" alt="" class="w-50 mb-4">
+                                        </a>
+                                        <h4 class="login-heading mb-4">Lengkapi Form dibawah ini!</h4>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+                                        {{-- Form --}}
+                                        <form action="{{ route('register') }}" method="post">
+                                            @csrf
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+                                            <div class="form-group mb-3">
+                                                <label for="email">Name</label>
+                                                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
 
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                                                @error('name')
+                                                    <span class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group mb-3">
+                                                <label for="email">Email</label>
+                                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                                                @error('email')
+                                                    <span class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group mb-3">
+                                                <label for="password">Password</label>
+                                                <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+                                                @error('password')
+                                                    <span class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group mb-3">
+                                                <label for="password_confirmation">Konfirmasi Password</label>
+                                                <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" name="password_confirmation">
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
+                                                @error('password_confirmation')
+                                                    <span class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </span>
+                                                @enderror
+                                            </div>
 
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
+                                            <div class="form-group d-flex justify-content-between align-items-center mb-3">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                                    <label for="customCheck1" class="custom-control-label">Ingat Saya?</label>
+                                                </div>
+                                                <a href="" class="small mt-1 text-muted">Lupa Password</a>
+                                            </div>
+
+                                            <div>
+                                                <button class="btn btn-lg btn-primary btn-login mb-2">
+                                                    <i class="fas fa-sign-in-alt"> Daftar</i>
+                                                </button>
+                                            </div>
+
+                                            <div class="text-center mt-3">
+                                                <div class="text-muted">                                                 
+                                                    Sudah punya akun silahkan login
+                                                    <a href="{{ route('login') }}" class="text-muted">disini</a>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </x-jet-label>
-                </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
+                    </div>
+                
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+    @endsection
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link">
       <img src="{{ Storage::disk('public')->url($setting->path_image ?? '') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
